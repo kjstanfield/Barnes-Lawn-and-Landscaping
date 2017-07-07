8092a8458479915e3587bf31fcd3bd0925d321ab
Kyle Stanfield <kjstan@gmail.com>
2017-07-06 20:01:05 -0400

    Added Map, Added and styled contact, adjusted about section text.

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function initMap() {
+    var louisville = {lat: 38.178399, lng: -85.740489};
+    var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: louisville
+    });
+}
