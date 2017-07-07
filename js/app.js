3c0a7d94dd19c4c119be13431f17ca8387a9b603
Kyle Stanfield <kjstan@gmail.com>
2017-07-07 05:58:56 -0400

    Added slick carousel, adjusted google map

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 function initMap() {
-    var louisville = {lat: 38.178399, lng: -85.740489};
+    var louisville = {lat: 38.200178, lng: -85.750351};
     var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 11,
+    zoom: 12,
     center: louisville,
     scrollwheel: false
     });
 }
+
+$(document).ready(function(){
+  $('.gallerybox').slick({
+      accessibility: true,
+      speed: 500,
+      autoplay: true,
+      dots: true,
+      fade: true,
+      adaptiveHeight: true
+  });
+});
