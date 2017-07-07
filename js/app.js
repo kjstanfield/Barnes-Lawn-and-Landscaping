ec99aa7f361f4ae83b2d3bb3c1852edc7eaa5372
Kyle Stanfield <kjstan@gmail.com>
2017-07-07 01:33:12 -0400

    Added some service images, changed CSS sizing.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ function initMap() {
     var louisville = {lat: 38.178399, lng: -85.740489};
     var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 11,
-    center: louisville
+    center: louisville,
+    scrollwheel: false
     });
 }
